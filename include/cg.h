1eb73573efa74b55443486c2f8fced974522fd1a
nathanrodrig <143113149+nathan2005-maker@users.noreply.github.com>
2026-07-23 14:47:11 -0300

    feat(cg) Criando estrutura base code generation

---
+++ include/cg.h
@@ -0,0 +1,18 @@
+#ifndef CG_H
+#define CG_H
+
+
+/*
+     E verificado se a quantidade (COUNT) atingiu a capacidade (CAPACITY) do array dinâmico (DA).
+     Se estiver realoca utilizando realloc para dobrar (*2) e valida utilizando ASSERT se a realocação foi bem-sucedida.
+     Dado gravado no ELEMENT e incrementa a quantidade (COUNT) do array dinâmico (DA).
+*/
+
+#include "pch.h"
+#nclude "ast.h"
+#include "sv.h"
+
+
+
+
+#endif // DA_H
